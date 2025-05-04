79daa9bb8f502de19898d21727a55b15ef5c8870
floriankacurri <floriankacurri10@gmail.com>
2025-05-04 23:33:16 +0200

    Update nav.js

--- javascript/nav.js
+++ javascript/nav.js
@@ -5,28 +5,31 @@ document.addEventListener("DOMContentLoaded", function(ev) {
 
     header.classList.add('header');
     header.innerHTML = `
-        <a href='#' class='logo'>
-            <img src='/Users/arlindhajdari/Desktop/projekt_web_final/media/LOGOOO.png' alt='' style='height: 60px; width: 60px;'>
-        </a>
-        <nav class='headbar'>
-            <a href='index.html'>Kryefaqja</a>
-            <a href='#'>Produktet</a>
-            <a href='#'>Sherbimet</a>
-            <a href='medical_advice.html'>Linja e Kujdesit</a>
-            <a href='na_kontaktoni.html'>Na kontaktoni</a>
-        </nav>
-        <div class='menubtn'>
-            <button><i class='fa fa-user'></i> <span style="padding-left: 5px;">Hyr / Regjistrohu</span></button>
-        </div>`;
+        <a href="../html/index.html"><img src="../media/LOGOOO.png" alt="Logo-Farmacia_Jote" id="logo" style="height: 60px;"></a>
+        <nav>
+            <a href="../html/index.html">Faqja Kryesore</a>
+            <a href="../html/kategorite.html">Produktet</a>
+            <a href="../html/sherbimet.html">Sherbimet</a>
+            <a href="../html/rreth_nesh.html">Rreth Nesh</a>
+            <a href="../html/na_kontaktoni.html">Na kontaktoni</a>
+            <a href="../html/takim_me_farmacistin.html">Takim me Farmacistin </a>
+            <a href="#"><i class="fa fa-user-o" aria-hidden="true"></i><span class="login-span" style="padding-left: 5px;">Hyr / Regjistrohu</span></a>
+            <a href="../html/chat_me_farmacistin.html">
+              <i class="fa fa-commenting-o"></i>
+            </a>
+            <a href="../html/karroca.html">
+              <i class="fa fa-shopping-cart"></i>
+            </a>
+         </nav>`;
 
     body.prepend(header);
 
     const currentUser = localStorage.getItem('logedInUser') || '';
     if (currentUser.length){
-        document.querySelector('.header .menubtn button span').innerText = ` ${currentUser}`;
+        document.querySelector('.header a .login-span').innerText = ` ${currentUser}`;
     }
 
-    document.querySelector('.header .menubtn').onclick = (ev) => {
+    document.querySelector('.header a .login-span').onclick = (ev) => {
         if (currentUser.length){
             let response = confirm('Deshironi te dilni nga llogaria juaj ?');
             console.log(response);
